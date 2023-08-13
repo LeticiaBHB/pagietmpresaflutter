ee28517669c91dc2f2dec3df4432a99e1a1e8bcb
Leticia <124852398+LeticiaBHB@users.noreply.github.com>
2023-08-13 15:12:18 -0300

    Update cliente.dart

--- lib/cliente.dart
+++ lib/cliente.dart
@@ -10,7 +10,6 @@ class Cliente extends StatefulWidget {
 class _ClienteState extends State<Cliente> {
   @override
   Widget build(BuildContext context) {
-    double imageWidth = MediaQuery.of(context).size.width * 0.4;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -29,38 +28,64 @@ class _ClienteState extends State<Cliente> {
           ),
           SingleChildScrollView(
             child: Container(
-              color: Colors.white.withOpacity(0.8),
               padding: EdgeInsets.all(16),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: Image.asset(
-                          'assets/cliente.jpeg',
-                          width: 100,
-                          height: 100,
-                          alignment: Alignment.center,
+                  Container(
+                    color: Colors.white.withOpacity(0.8),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Image.asset(
+                            'assets/cliente.jpeg',
+                            width: 100,
+                            height: 100,
+                            alignment: Alignment.center,
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 10),
-                          child: Text(
-                            'Nossos Clientes',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.deepOrangeAccent,
+                        Expanded(
+                          flex: 2,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: Text(
+                              'Nossos Clientes',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.deepOrangeAccent,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                  Column()
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 50, 10, 50),
+                    color: Colors.white.withOpacity(0.8),
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: [
+                        Image.asset('assets/clientes/cliente1.jpeg'),
+                        Image.asset('assets/clientes/cliente2.jpeg'),
+                        Image.asset('assets/clientes/cliente3.jpeg'),
+                        Image.asset('assets/clientes/cliente4.jpeg'),
+                        Image.asset('assets/clientes/cliente5.jpeg'),
+                        Image.asset('assets/clientes/cliente6.jpeg'),
+                        Image.asset('assets/clientes/cliente7.jpeg'),
+                        Image.asset('assets/clientes/cliente8.jpeg'),
+                        Image.asset('assets/clientes/cliente9.jpeg'),
+                        Image.asset('assets/clientes/cliente10.jpeg'),
+                        Image.asset('assets/clientes/cliente11.jpeg'),
+                        Image.asset('assets/clientes/cliente12.jpeg'),
+                        Image.asset('assets/clientes/cliente13.jpeg'),
+                        Image.asset('assets/clientes/cliente14.jpeg'),
+                        Image.asset('assets/clientes/cliente15.jpeg'),
+                        Image.asset('assets/clientes/cliente16.jpeg'),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
